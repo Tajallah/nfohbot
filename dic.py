4ab5614dee1d1a6bd88826047b7954a1ed5bacf8
Ana <tajallah.sims@gmail.com>
2019-07-25 13:10:37 -0700

    wotd is now self contained, ready to be called by a cron

--- dic.py
+++ dic.py
@@ -1,7 +1,16 @@
+import discord
 from bs4 import BeautifulSoup as bs
 from urllib.request import Request, urlopen
 
+f = open("token", "r")
+token = f.readline().strip()
+
+client = discord.Client()
+
+channel = 413883696530325506
+
 def wotd():
+    wotd_header = "The word of the day is: \n\n"
     url = "https://www.dictionary.com/e/word-of-the-day/"
     req = Request(url)
     html = urlopen(req).read()
@@ -13,6 +22,13 @@ def wotd():
     wotd = block.h1.get_text()
     pos = block.find(class_="wotd-item__definition__pronunciation").get_text().strip()
     definition = block.find(class_="wotd-item__definition__text").get_text().strip("\"").strip()
-    fnl = "*" + wotd + "* \n" + pos +  " \n**" + definition + "**"
+    fnl = wotd_header + "**" + wotd + "** \n\n" + pos +  " \n\n*" + definition + "*"
     print(fnl)
     return(fnl)
+
+@client.event
+async def on_ready():
+    ch = client.get_channel(channel)
+    await ch.send(wotd())
+
+client.run(token)
